c532a7ebfafda1b8d427ea79023a8d463f549689
lintang <lintangmochz@gmail.com>
2022-08-21 17:27:39 +0700

    edit import model, edit res.status, tambah pernjagaan

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -1,18 +1,24 @@
-const User = require('../models').User;
-const Order = require('../models').Order;
+const { User } = require('../models');
+const { Order } = require('../models');
 
 const add = async (req, res) => {
     try {
+        const userId = await User.findOne({ where: { id: req.body.userId } })
+        if (!userId) {
+            return res.json({ message: 'User tidak ditemukan' })
+        }
+
         let result;
         result = await Order.create({
             dateOrder: req.body.dateOrder,
             userId: req.body.userId,
             status: req.body.status
         })
-        return res.status(200).send(result)
-    } catch {
-        return res.status(404).send({
-            message: 'Order gagal ditambahkan.'
+        return res.status(200).json(result)
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Order gagal ditambahkan.',
+            err: err
         })
     }
 }
@@ -21,9 +27,9 @@ const list = async (req, res) => {
     try {
         let result = await Order.findAll()
 
-        return res.status(200).send(result)
+        return res.status(200).json(result)
     } catch {
-        return res.status(404).send({
+        return res.status(500).json({
             message: 'Server is error.'
         })
     }
@@ -42,13 +48,13 @@ const getById = async (req, res) => {
         })
 
         if (!order) {
-            return res.status(401).send({
+            return res.status(500).json({
                 message: 'Order tidak ditemukan.'
             })
         }
-        return res.status(200).send(order)
+        return res.status(200).json(order)
     } catch {
-        return res.status(404).send({
+        return res.status(500).json({
             message: 'Server is error.'
         })
     }
@@ -56,10 +62,15 @@ const getById = async (req, res) => {
 
 const update = async (req, res) => {
     try {
+        const userId = await User.findOne({ where: { id: req.body.userId } })
+        if (!userId) {
+            return res.json({ message: 'User tidak ditemukan' })
+        }
+
         let order = await Order.findByPk(req.params.id)
 
         if (!order) {
-            return res.status(401).send({
+            return res.status(500).json({
                 message: 'Order tidak ditemukan.'
             })
         }
@@ -68,12 +79,12 @@ const update = async (req, res) => {
             userId: req.body.userId,
             status: req.body.status
         })
-        return res.status(200).send({
+        return res.status(200).json({
             message: 'Order berhasil diperbarui.',
             result: order
         })
     } catch {
-        return res.status(404).send({
+        return res.status(500).json({
             message: 'Server is error.'
         })
     }
@@ -83,17 +94,17 @@ const deleteById = async (req, res) => {
     try {
         let order = await Order.findByPk(req.params.id);
         if (!order) {
-            return res.status(401).send({
+            return res.status(500).json({
                 message: 'Order tidak ditemukan.'
             })
         }
-        
+
         await order.destroy()
-        return res.status(201).send({
+        return res.status(200).json({
             message: 'Order berhasil dihapus.'
         })
     } catch {
-        return res.status(404).send({
+        return res.status(500).json({
             message: 'Server is error.'
         })
     }
